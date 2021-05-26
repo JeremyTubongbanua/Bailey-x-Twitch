a4d33f41cd39692eb2fdd49fcb37701a20c2dc08
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-05-26 00:25:33 -0400

    Fixed whitelist and added "waters" counter

--- stream-labs/chat-bot.js
+++ stream-labs/chat-bot.js
@@ -1,5 +1,10 @@
 const tmi = require("tmi.js");
+const jsonfile = require('jsonfile');
+const file = './stats.json';
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+// subject to change
+const baseUrl = "http://192.168.0.104/";
 
 // Define configuration options
 const opts = {
@@ -30,6 +35,10 @@ for (var i in read) {
 }
 console.log(whitelist);
 
+var cooldowns = [
+	
+];
+
 // Called every time a message comes in
 function onMessageHandler(target, context, msg, self) {
 	if (self) {
@@ -52,25 +61,42 @@ function onMessageHandler(target, context, msg, self) {
 			"CPU: Ryzen 5 3600, 1050 Ti 4GB, 16GB RAM, Samsung 970 500GB SSD";
 		client.say(target, specs);
 	} else if (commandName === "!debug") {
+		client.say(target, "Boop!");
 	} else if (commandName === "!water") {
-		if(whitelist.includes(noHashTagName)) {
-			const baseUrl = "http://192.168.0.104/"
-			var xhr = new XMLHttpRequest();
-			var url = baseUrl + "command";
-			xhr.open("GET", url);
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState === 4) {
-					console.log(xhr.status);
-					console.log(xhr.responseText);
+		if (whitelist.includes(noHashTagName)) {
+			sendCommandHTTPRequest();
+			jsonfile.readFile(file, (err, obj) => {
+				const prf = obj[noHashTagName];
+				if (prf == undefined) {
+					// initialize them
+					obj[noHashTagName] = { waters: 1 }
+					jsonfile.writeFile(file, obj);
+					client.say(target, `Thanks for watering Bailey! (1 waters)`);
+				} else {
+					const newWaters = obj[noHashTagName].waters + 1;
+					obj[noHashTagName].waters = newWaters;
+					jsonfile.writeFile(file, obj);
+					client.say(target, `Thanks for watering Bailey! (${newWaters} waters)`);
 				}
-			};
-			xhr.send();
-			client.say(target, `Thanks for watering Bailey!`)
+			});
 		} else {
-			client.say(target, `@${noHashTagName} You are not whitelisted. Claim the "Give me power" channel points reward for 1000 fruit berries to be whitelisted.`);
+			client.say(target, `You are not whitelisted. Claim the "Give me power" channel points reward.`);
 		}
 	}
 	console.log(`* Executed ${commandName} command`);
+}
+
+function sendCommandHTTPRequest() {
+	var xhr = new XMLHttpRequest();
+	var url = baseUrl + "command";
+	xhr.open("GET", url);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+		}
+	};
+	xhr.send();
 }
 
 // Function called when the "dice" command is issued
